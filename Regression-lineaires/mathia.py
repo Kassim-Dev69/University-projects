e6717234a987080247e4b40bb34944a76df41f76
Kassim Genesis <tchitembogenesis@gmail.com>
2026-05-18 11:50:37 +0100

    Update file loading to use 'housing.csv'

--- Regression-lineaires/mathia.py
+++ Regression-lineaires/mathia.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 try:
-  df = pd.read.csv('')
-except FileNotFoundError:
-  print("Erreur : Le fichier 'anime_dataset.csv' n'existe pas !")
+  df = pd.read.csv('housing.csv')
+except:
+  print("Erreur : Le fichier 'housing.csv' n'existe pas !")
   exit()
+
+print("--- Aperçu des données ---")
+print(df.head(10))
